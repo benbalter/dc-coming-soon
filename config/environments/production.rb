93c9e8119e264446905918ee7fb94368e301252e
Ben Balter <ben.balter@github.com>
2015-12-17 15:31:13 -0500

    try better caching for rack::cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,10 @@ Rails.application.configure do
   }
   config.static_cache_control = "public, max-age=2592000"
 
+  RestClient.enable Rack::Cache,
+    :metastore    => client,
+    :entitystore  => client
+    
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
